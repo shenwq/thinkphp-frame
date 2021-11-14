5c41337c2fc371c44a353045695caead79f4851b
John.Shen <John.Shen@foxmail.com>
2021-11-14 20:54:19 +0800

    增加登录认证处理

---
+++ src/middleware/CheckAuth.php
@@ -0,0 +1,48 @@
+<?php
+declare (strict_types=1);
+
+namespace ffhome\frame\middleware;
+
+use \Closure;
+use \ffhome\frame\traits\JumpTrait;
+use ffhome\frame\service\AuthService;
+use think\Request;
+
+/**
+ * 检测用户登录和节点权限
+ * Class CheckAdmin
+ * @package app\admin\middleware
+ */
+class CheckAuth
+{
+    use JumpTrait;
+
+    public function handle(Request $request, Closure $next)
+    {
+        $authConfig = config('auth');
+        $authService = new AuthService();
+        $currentNode = $authService->currentNode();
+        // 验证登录
+        if (in_array($currentNode, $authConfig['no_login'])) {
+            return $next($request);
+        }
+
+        $userId = $authService->currentUserId();
+        // 验证登录
+        if (empty($userId)) {
+            $this->error(lang('common.login_first'), [], url('admin/login/index')->build());
+        }
+        // 验证权限
+        if (in_array($currentNode, $authConfig['no_auth'])) {
+            return $next($request);
+        }
+
+        // 验证权限
+        if (!$authService->check($currentNode)) {
+            trace(lang('common.not_auth') . $currentNode);
+            $this->error(lang('common.not_auth'));
+        }
+
+        return $next($request);
+    }
+}
