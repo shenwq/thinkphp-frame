545742a0c3f6d6b70b080c9568276fb4ab06f89f
John.Shen <John.Shen@foxmail.com>
2021-11-14 20:32:03 +0800

    增加日志中间件处理

---
+++ src/middleware/SystemLog.php
@@ -0,0 +1,58 @@
+<?php
+declare (strict_types=1);
+
+namespace ffhome\frame\middleware;
+
+use ffhome\common\util\CommonUtil;
+use ffhome\frame\service\AuthService;
+use ffhome\frame\service\SystemLogService;
+use think\Request;
+
+/**
+ * 系统操作日志中间件
+ */
+class SystemLog
+{
+
+    /**
+     * 敏感信息字段，日志记录时需要加密
+     * @var array
+     */
+    protected $sensitiveParams = [
+        'password',
+        'password_again',
+    ];
+
+    public function handle(Request $request, \Closure $next)
+    {
+        if ($request->isAjax()) {
+            $method = strtolower($request->method());
+            if (in_array($method, ['get', 'post', 'put', 'delete'])) {
+                $url = $request->url();
+                $ip = CommonUtil::getRealIp();
+                $params = $request->param();
+                $fingerprint = cookie('fingerprint');
+                if (isset($params['s'])) {
+                    unset($params['s']);
+                }
+                foreach ($params as $key => $val) {
+                    in_array($key, $this->sensitiveParams) && $params[$key] = CommonUtil::password($val);
+                }
+                $data = [
+                    'user_id' => (new AuthService())->currentUserId(),
+                    'url' => $url,
+                    'method' => $method,
+                    'ip' => $ip,
+                    'content' => json_encode($params, JSON_UNESCAPED_UNICODE),
+                    'useragent' => $_SERVER['HTTP_USER_AGENT'],
+                    'create_time' => date('Y-m-d H:i:s'),
+                ];
+                if (!empty($fingerprint)) {
+                    $data['fingerprint'] = $fingerprint;
+                }
+                (new SystemLogService())->save($data);
+            }
+        }
+        return $next($request);
+    }
+}
---
+++ src/service/SystemLogService.php
@@ -0,0 +1,101 @@
+<?php
+declare (strict_types=1);
+
+namespace ffhome\frame\service;
+
+use think\facade\Config;
+use think\facade\Db;
+
+/**
+ * 系统日志表
+ */
+class SystemLogService
+{
+    /**
+     * 表前缀
+     * @var string
+     */
+    protected $tablePrefix;
+
+    /**
+     * 表后缀
+     * @var string
+     */
+    protected $tableSuffix;
+
+    /**
+     * 表名
+     * @var string
+     */
+    protected $tableName;
+
+    /**
+     * 构造方法
+     */
+    public function __construct()
+    {
+        $this->tablePrefix = Config::get('database.connections.mysql.prefix');
+        $this->tableSuffix = date('Ym', time());
+        $this->tableName = "{$this->tablePrefix}system_log_{$this->tableSuffix}";
+        return $this;
+    }
+
+
+    /**
+     * 保存数据
+     * @param $data
+     * @return bool|string
+     */
+    public function save($data)
+    {
+        $this->detectTable();
+        Db::startTrans();
+        try {
+            Db::table($this->tableName)->insert($data);
+            Db::commit();
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        }
+        return true;
+    }
+
+    /**
+     * 检测数据表
+     * @return bool
+     */
+    protected function detectTable()
+    {
+        $cacheTable = cache("log_table_{$this->tableName}");
+        if (empty($cacheTable)) {
+            $check = Db::query("show tables like '{$this->tableName}'");
+            if (empty($check)) {
+                $sql = $this->getCreateSql();
+                Db::execute($sql);
+            }
+            cache("log_table_{$this->tableName}", true);
+        }
+        return true;
+    }
+
+    /**
+     * 根据后缀获取创建表的sql
+     * @return string
+     */
+    protected function getCreateSql()
+    {
+        return <<<EOT
+CREATE TABLE `{$this->tableName}` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `fingerprint` varchar(32) NOT NULL DEFAULT '' COMMENT '浏览器指纹',
+  `create_time` datetime NOT NULL DEFAULT '2000-01-01 00:00:00' COMMENT '操作时间',
+  `url` varchar(1500) NOT NULL DEFAULT '' COMMENT '操作页面',
+  `method` varchar(50) NOT NULL COMMENT '请求方法',
+  `ip` varchar(50) NOT NULL DEFAULT '' COMMENT 'IP',
+  `user_id` bigint unsigned NOT NULL DEFAULT 0 COMMENT '人员ID',
+  `content` text NOT NULL COMMENT '内容',
+  `useragent` varchar(255) DEFAULT '' COMMENT 'User-Agent',
+  PRIMARY KEY (`id`)
+) COMMENT='后台操作日志表 - {$this->tableSuffix}';
+EOT;
+    }
+}
