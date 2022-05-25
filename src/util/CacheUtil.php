adf40741561cbb980da4cc929ba8e3d8b8cc61ac
John.Shen <john.shen@foxmail.com>
2022-05-25 10:08:19 +0800

    缓存增加缓存类型参数处理

--- src/util/CacheUtil.php
+++ src/util/CacheUtil.php
@@ -13,14 +13,23 @@ class CacheUtil
      * @param \Closure $fn 缓存找不到时调用，返回数据放到缓存中
      * @param string $tag 根据tag批量清除缓存处理
      * @param int $expire 有效时间（秒）
+     * @param string $store 缓存类型 如redis,file
      * @return mixed 从缓存中取出的值
      */
-    public static function get(string $name, \Closure $fn, string $tag = 'common', int $expire = null)
+    public static function get(string $name, \Closure $fn, string $tag = 'common', int $expire = null, string $store = '')
     {
-        $data = Cache::get($name);
+        if (empty($store)) {
+            $data = Cache::get($name);
+        } else {
+            $data = Cache::store($store)->get($name);
+        }
         if (empty($data)) {
             $data = $fn();
-            Cache::tag($tag)->set($name, $data, $expire);
+            if (empty($store)) {
+                Cache::tag($tag)->set($name, $data, $expire);
+            } else {
+                Cache::store($store)->tag($tag)->set($name, $data, $expire);
+            }
         }
         return $data;
     }
