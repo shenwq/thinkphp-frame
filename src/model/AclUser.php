b4bf962bc0ab9847e0fb649a5aa0773c9341440f
John.Shen <john.shen@foxmail.com>
2022-12-02 09:35:29 +0800

    增加按角色获取人员列表功能

--- src/model/AclUser.php
+++ src/model/AclUser.php
@@ -3,7 +3,25 @@ declare (strict_types=1);
 
 namespace ffhome\frame\model;
 
+use ffhome\frame\util\CacheUtil;
+use think\facade\Db;
+
 class AclUser extends BaseModel
 {
     const MODEL_NAME = 'acl_user';
+
+    public static function getListByRoleId($roleId)
+    {
+        return CacheUtil::get("userListByRoleId_{$roleId}", function () use ($roleId) {
+            return Db::name(self::MODEL_NAME)->alias('u')
+                ->leftJoin(AclUserRole::MODEL_NAME . ' ur', 'ur.user_id=u.id')
+                ->where([
+                    ['ur.role_id', '=', $roleId],
+                    ['u.status', '=', self::ENABLE],
+                    ['u.delete_time', 'exp', Db::raw('is null')],
+                ])
+                ->order(['id' => 'desc'])
+                ->column('u.nick_name', 'u.id');
+        }, self::MODEL_NAME);
+    }
 }
